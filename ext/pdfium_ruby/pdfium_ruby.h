ea80d6921473320803dc8ffc57154b94ed649567
Ted Han <ted@knowtheory.net>
2015-01-26 15:37:41 -0600

    remove test line.

--- ext/pdfium_ruby/pdfium_ruby.h
+++ ext/pdfium_ruby/pdfium_ruby.h
@@ -13,6 +13,7 @@ void Define_Document();
 void Define_Page();
 void Define_PageSet();
 
+// helper function for printing string literals for debugging purposes
 void inline ruby_puts(const char* str) { rb_funcall(rb_cObject, rb_intern("puts"), 1, rb_str_new_cstr(str)); }
 
 #endif
