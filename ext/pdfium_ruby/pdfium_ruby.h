f57b70c85808c0946e63d87f7947fb12d60fe23a
Ted Han <ted@knowtheory.net>
2015-01-26 15:13:23 -0600

    Create a type to cast Ruby's C functions to a thing C++ is happy with.

--- ext/pdfium_ruby/pdfium_ruby.h
+++ ext/pdfium_ruby/pdfium_ruby.h
@@ -1,7 +1,16 @@
+#ifndef __PDFIUM_RUBY_H__
+#define __PDFIUM_RUBY_H__
+
 extern "C" {
   #include "ruby.h"
 }
 
+// Inspired by https://github.com/jasonroelofs/rice/blob/1740a6d12c99fce8c21eda3c5385738318ab9172/rice/detail/ruby.hpp#L33-L37
+// Casts C functions into a type that C++ is happy calling
+extern "C" typedef VALUE (*CPP_RUBY_METHOD_FUNC)(ANYARGS);
+
 void Define_Document();
 void Define_Page();
 void Define_PageSet();
+
+#endif
