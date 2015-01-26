ce8b9c231405b95d1664dc3548044d6757af516d
Ted Han <ted@knowtheory.net>
2015-01-26 14:29:41 -0600

    Fix main file's init function for C++, and comment out unimplemented functions.

--- ext/pdfium_ruby/pdfium_ruby.cpp
+++ ext/pdfium_ruby/pdfium_ruby.cpp
@@ -1,10 +1,12 @@
 #include "pdfium_ruby.h"
 
+extern "C"
 void Init_pdfium_ruby (void) {
+  // Define `PDFium` module as a namespace for all of our other objects
   VALUE rb_PDFium = rb_define_module("PDFium");
   
   // Define `Document` and `Page` classes
   Define_Document();
-  Define_Page();
-  Define_PageSet();
+  //Define_Page();
+  //Define_PageSet();
 }
