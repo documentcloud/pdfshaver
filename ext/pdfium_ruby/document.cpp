6b6f8dfe39ea9c98046f047c2e763fe23dbeeb91
Ted Han <ted@knowtheory.net>
2015-01-27 17:30:20 -0600

    set up guards/checking system for destroying documents (cause PDFium segfaults if you release a document before releasing its pages)

--- ext/pdfium_ruby/document.cpp
+++ ext/pdfium_ruby/document.cpp
@@ -18,15 +18,18 @@ Document::~Document() {
   if (document) { FPDF_CloseDocument(document); }
 }
 
-int Document::length() {
-  return FPDF_GetPageCount(document);
-}
+int
+Document::length() { return FPDF_GetPageCount(document); }
 
-//void freeUnlessPagesAreOpen() {
-//  // once the document is no longer being used, and none of its child pages are open
-//  // it's safe to destroy.
-//  if (free_once_pages_are_closed && open_pages.empty()) { delete this; }
-//}
+void
+Document::flagDocumentAsReadyForRelease() { this->free_once_pages_are_closed = true; }
+
+void
+Document::destroyUnlessPagesAreOpen() {
+  // once the document is no longer being used, and none of its child pages are open
+  // it's safe to destroy.
+  if (free_once_pages_are_closed /*&& open_pages.empty()*/) { delete this; }
+}
 
 /********************************************
 * Ruby class definition and initialization
@@ -47,10 +50,6 @@ VALUE initialize_document_internals(int arg_count, VALUE* args, VALUE self) {
   // use Ruby's argument scanner to pull out a required
   // `path` argument and an optional `options` hash.
   int number_of_args = rb_scan_args(arg_count, args, "11", &path, &options);
-  //printf("Number of Arguments: %d\n", number_of_args);
-  //Check_Type(path, T_STRING);
-  //rb_sprintf("WTF: %"PRIsVALUE"", args);
-  //rb_funcall(rb_cObject, rb_intern("puts"), 1, rb_sprintf("%"PRIsVALUE" inspected: %+"PRIsVALUE"", path, path));
   
   // attempt to open document.
   // path should at this point be validated & known to exist.
@@ -59,13 +58,14 @@ VALUE initialize_document_internals(int arg_count, VALUE* args, VALUE self) {
   // Get the PDFium namespace and define the `Document` class inside it.
   VALUE rb_PDFium = rb_const_get(rb_cObject, rb_intern("PDFium"));
   VALUE rb_PDFium_Document = rb_const_get(rb_PDFium, rb_intern("Document"));
-  Data_Wrap_Struct(rb_PDFium_Document, NULL, free_document_when_safe, document);
+  Data_Wrap_Struct(rb_PDFium_Document, NULL, destroy_document_when_safe, document);
   
+  // get the document length and store it as an instance variable on the class.
   rb_ivar_set(self, rb_intern("@length"), INT2FIX(document->length()));
   return self;
 }
 
-static void free_document_when_safe(Document* document) {
-  //document->free_once_pages_are_closed = true;
-  delete document;
+static void destroy_document_when_safe(Document* document) {
+  document->flagDocumentAsReadyForRelease();
+  document->destroyUnlessPagesAreOpen();
 }
--- ext/pdfium_ruby/document.h
+++ ext/pdfium_ruby/document.h
@@ -12,19 +12,26 @@ class Document {
     // constructor
     Document(VALUE path);
 
+    // wrapper for PDFium's pageCount
+    int length();
+    
+    // flag to set instances as ready to be disposed of
+    // pending ensuring all its pages have been first closed.
+    void flagDocumentAsReadyForRelease();
+    
+    // a guard for the destructor.
+    void destroyUnlessPagesAreOpen();
+
     // destructor
     ~Document();
     
-    int length();
-
   private:
     FPDF_DOCUMENT document;
     bool free_once_pages_are_closed;
     //std::unordered_set<Page*> open_pages;
-    //void freeUnlessPagesAreOpen();
 };
 
-static void free_document_when_safe(Document* document);
+static void destroy_document_when_safe(Document* document);
 
 VALUE initialize_document_internals(int arg_count, VALUE* args, VALUE self);
 
