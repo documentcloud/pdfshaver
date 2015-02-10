3c49a27121556c0967a61ffc0578f4b0fe4b8f86
Ted Han <ted@knowtheory.net>
2015-02-10 12:35:04 -0600

    update extconf so that it compiles on ubuntu

--- ext/pdfium_ruby/extconf.rb
+++ ext/pdfium_ruby/extconf.rb
@@ -18,42 +18,44 @@ HEADER_DIRS = append_pdfium_directory_to %w[
 dir_config("pdfium", HEADER_DIRS, LIB_DIRS)
 
 LIB_FILES= %w[
+  javascript
   bigint
   freetype
   fpdfdoc
   fpdftext
   formfiller
-  javascript
-  v8_base
+  icudata
+  icuuc
+  icui18n
   v8_libbase
+  v8_base
   v8_snapshot
   v8_libplatform
   jsapi
-  icui18n
-  icuuc
-  icudata
   pdfwindow
   fxedit
-  fxcodec
   fxcrt
+  fxcodec
   fpdfdoc
-  fpdfapi
   fdrm
   fxge
-  pthread
+  fpdfapi
+  freetype
   pdfium
+  pthread
   freeimage
 ]
+
 LIB_FILES.each do | lib |
   have_library(lib) or abort "Couldn't find library lib#{lib} in #{LIB_DIRS.join(', ')}"
 end
 
-# Core Graphics needed on macs to run PDFium.
-if `uname`.chomp == 'Darwin' then
-
+if RUBY_PLATFORM =~ /darwin/
+  have_library('objc')
   FRAMEWORKS = %w{AppKit CoreFoundation}
-
   $LDFLAGS << FRAMEWORKS.map { |f| " -framework #{f}" }.join
+else
+  $CPPFLAGS += " -fPIC -std=c++11"
 end
 
 create_makefile "pdfium_ruby"
