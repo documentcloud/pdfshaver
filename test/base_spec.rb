2c973b4c2662a323a7cf1b88f9315fd4570049a7
Ted Han <ted@knowtheory.net>
2015-02-09 21:34:50 -0600

    Have a pile of specs about rendering.

--- test/base_spec.rb
+++ test/base_spec.rb
@@ -73,11 +73,59 @@ describe PDFium::Page do
   end
   
   describe "instance methods" do
-    before { @page = PDFium::Page.new(@document, 1) }
+    before do
+      @page = PDFium::Page.new(@document, 1)
+      @output_path = File.join OUTPUT, 'image_render_test.gif'
+    end
+
     it "should have the right width, height and aspect ratio" do
       @page.width.must_be_kind_of Integer
       @page.height.must_be_kind_of Integer
       @page.aspect.must_be_kind_of Float
+    end
+    
+    it "should render an image and write it out to disk" do
+      @page.render(@output_path).must_equal true
+      File.exists?(@output_path).must_equal true
+    end
+    
+    it "should raise an error if output path is missing" do
+      Proc.new{ @page.render }.must_raise ArgumentError
+    end
+    
+    it "should use default page size if dimensions aren't specified" do
+      @page.render @output_path
+      FastImage.size(@output_path).zip([@page.width, @page.height]).each do |actual, expected|
+        actual.must_equal expected
+      end
+    end
+    
+    it "should scale image when height is specified" do
+      target_height = 1000
+      @page.render @output_path, height:target_height, width:nil
+      width, height = FastImage.size @output_path
+      height.must_equal target_height
+    end
+
+    it "should scale image when width is specified" do
+      target_width = 1000
+      @page.render @output_path, height:nil, width:target_width
+      width, height = FastImage.size @output_path
+      width.must_equal target_width
+    end
+    
+    it "should scale image when height and width are specified" do
+      target_height = target_width = 1000
+      @page.render @output_path, height:target_height, width:target_width
+      width, height = FastImage.size @output_path
+      width.must_equal target_width
+      height.must_equal target_height
+    end
+    
+    it "should preserve aspect ratio when scaling" do
+      @page.render @output_path, height:1000, width:nil
+      width, height = FastImage.size @output_path
+      (width.to_f / height).must_be_within_delta @page.aspect, 0.001
     end
   end
   
