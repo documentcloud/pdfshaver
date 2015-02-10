b2a897e14e5d6bba9a4afb876503d617a0c22956
Ted Han <ted@knowtheory.net>
2015-02-09 21:12:10 -0600

    Add fastimage gem for testing.

--- pdfium.gemspec
+++ pdfium.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rake'
   s.add_development_dependency 'rake-compiler'
   s.add_development_dependency 'minitest'
+  s.add_development_dependency 'fastimage'
 end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -1,6 +1,10 @@
 require "minitest/autorun"
+require 'fastimage'
+require 'fileutils'
 
 HERE = File.expand_path File.dirname(__FILE__)
 FIXTURES = File.join(HERE, 'fixtures')
+OUTPUT = File.join(HERE, 'output')
+FileUtils.mkdir_p OUTPUT
 
 require File.join(HERE, '..', 'lib', 'pdfium')
