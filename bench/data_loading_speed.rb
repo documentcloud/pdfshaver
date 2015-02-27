b253d78edaee459bb5ea3784d1982f77c89b5277
Ted Han <ted@knowtheory.net>
2015-02-27 15:37:06 -0600

    Benchmark test which prove our code provides no speed improvements.

--- bench/data_loading_speed.rb
+++ bench/data_loading_speed.rb
@@ -1,13 +1,13 @@
 require_relative 'setup'
+require 'benchmark'
 
 here = File.dirname(__FILE__)
 path = File.join(here, '..', 'test', 'fixtures', 'uncharter.pdf')
-doc = PDFShaver::Document.new(doc_path)
+doc = PDFShaver::Document.new(path)
+out_dir = File.join(here, 'output', 'speed')
 
-doc.pages.each do |page|
-  full_smart_render(page, out_dir)
-end
-
-doc.pages.each do |page|
-  full_naive_render(page, out_dir)
+count = 10
+Benchmark.bm do |test|
+  test.report("naive"){ count.times{ doc.pages.each{ |page| full_naive_render(page, out_dir) } } }
+  test.report("smart"){ count.times{ doc.pages.each{ |page| full_smart_render(page, out_dir) } } }
 end
