04a2dc58eca6ea845646c6015dc02b78d8806a62
Ted Han <ted@knowtheory.net>
2015-02-27 14:35:04 -0600

    Docs explaining how C++ Pages work

--- ext/pdfium_ruby/page.cpp
+++ ext/pdfium_ruby/page.cpp
@@ -5,11 +5,11 @@
 * C++ Page definition
 *********************************************/
 
-// Make sure C++ pages, when created are marked as unopened.
+// When created make sure C++ pages are marked as unopened.
 Page::Page() { this->opened = false; }
 
-// When destroying a C++ Page, make sure to
-// dispose of the internals properly.
+// When destroying a C++ Page, make sure to dispose of the internals properly.
+// And notify the parent document that this page is no longer going to be used.
 Page::~Page() {
   if (this->opened) {
     this->unload();
@@ -17,12 +17,16 @@ Page::~Page() {
   }
 }
 
+// When the page is initialized through the Ruby lifecycle, store a reference
+// to its parent Document, the page number and notify the Document that this page
+// is available to be loaded.
 void Page::initialize(Document* document, int page_index) {
   this->document = document;
   this->page_index = page_index;
   this->document->notifyPageOpened(this);
 }
 
+// Load the page through PDFium and flag the document as currently open.
 bool Page::load() {
   if (!this->opened) {
     this->fpdf_page = FPDF_LoadPage(this->document->fpdf_document, this->page_index);
@@ -31,15 +35,19 @@ bool Page::load() {
   return this->opened;
 }
 
+// Unload the page (freeing the page's memory) and mark it as not open.
 void Page::unload() {
   if (this->opened){ FPDF_ClosePage(this->fpdf_page); }
   this->opened = false;
 }
 
+// readers for the page's dimensions.
 double Page::width(){ return FPDF_GetPageWidth(this->fpdf_page); }
 double Page::height(){ return FPDF_GetPageHeight(this->fpdf_page); }
 double Page::aspect() { return width() / height(); }
 
+// Render the page to a destination path with the dimensions
+// specified by width & height (or appropriate defaults).
 bool Page::render(char* path, int width, int height) {
   // If no height or width is supplied, render at natural dimensions.
   if (!width && !height) {
@@ -50,18 +58,16 @@ bool Page::render(char* path, int width, int height) {
   // infer the other by preserving page aspect ratio.
   if ( width && !height) { height = width  / this->aspect(); }
   if (!width &&  height) { width  = height * this->aspect(); }
-  //printf("Derp? %d, %d\n", width, height);
   
   // Create bitmap.  width, height, alpha 1=enabled,0=disabled
   bool alpha = false;
   FPDF_BITMAP bitmap = FPDFBitmap_Create(width, height, alpha);
   if (!bitmap) { return false; }
 
-  // fill all pixels with white for the background color
+  // and fill all pixels with white for the background color
   FPDFBitmap_FillRect(bitmap, 0, 0, width, height, 0xFFFFFFFF);
 
-  // Render a page to a bitmap in RGBA format
-  // args are: *buffer, page, start_x, start_y, size_x, size_y, rotation, and flags
+  // Render a page into the bitmap in RGBA format
   // flags are:
   //      0 for normal display, or combination of flags defined below
   //   0x01 Set if annotations are to be rendered
@@ -72,7 +78,8 @@ bool Page::render(char* path, int width, int height) {
   int rotation = 0;
   int flags = FPDF_PRINTING; // A flag defined in PDFium's codebase.
   FPDF_RenderPageBitmap(bitmap, this->fpdf_page, start_x, start_y, width, height, rotation, flags);
-
+  
+  // Calculate the page's stride.
   // The stride holds the width of one row in bytes.  It may not be an exact
   // multiple of the pixel width because the data may be packed to always end on a byte boundary
   int stride = FPDFBitmap_GetStride(bitmap);
@@ -89,16 +96,17 @@ bool Page::render(char* path, int width, int height) {
       return false;
   }
 
-  // Read the FPDF bitmap into a FreeImage bitmap.
+  // Hand off the PDFium bitmap data to FreeImage for additional processing.
   unsigned bpp        = 32;
   unsigned red_mask   = 0xFF0000;
   unsigned green_mask = 0x00FF00;
   unsigned blue_mask  = 0x0000FF;
   bool     topdown    = true;
   FIBITMAP *raw = FreeImage_ConvertFromRawBits(
-    (BYTE*)FPDFBitmap_GetBuffer(bitmap), width, height, stride, bpp, red_mask, green_mask, blue_mask, topdown);
+    (BYTE*)FPDFBitmap_GetBuffer(bitmap), width, height, stride, bpp, 
+    red_mask, green_mask, blue_mask, topdown);
 
-  // at this point we're done with the FPDF bitmap and can destroy it.
+  // With bitmap handoff complete the FPDF bitmap can be destroyed.
   FPDFBitmap_Destroy(bitmap);
 
   // Conversion to jpg or gif require that the bpp be set to 24
@@ -135,18 +143,44 @@ void Define_Page() {
   VALUE rb_PDFShaver = rb_const_get(rb_cObject, rb_intern("PDFShaver"));
   VALUE rb_PDFShaver_Page = rb_const_get(rb_PDFShaver, rb_intern("Page"));
   
+  // Define the C allocator function so that when a new PDFShaver::Page instance
+  // is created, our C/C++ data structures are initialized into the Ruby lifecycle.
   rb_define_alloc_func(rb_PDFShaver_Page, *page_allocate);
   
-  rb_define_method(rb_PDFShaver_Page, "render", CPP_RUBY_METHOD_FUNC(page_render), -1);
+  // Wire the C functions we need/want into Ruby land.
+  // We're using the CPP_RUBY_METHOD_FUNC to wrap functions for C++'s comfort.
   rb_define_private_method(rb_PDFShaver_Page, "initialize_page_internals", 
                             CPP_RUBY_METHOD_FUNC(initialize_page_internals),-1);
+  rb_define_method(rb_PDFShaver_Page, "render", CPP_RUBY_METHOD_FUNC(page_render), -1);
   rb_define_private_method(rb_PDFShaver_Page, "load_data",   CPP_RUBY_METHOD_FUNC(page_load_data), 0);
   rb_define_private_method(rb_PDFShaver_Page, "unload_data", CPP_RUBY_METHOD_FUNC(page_unload_data), 0);
 }
 
+// Create a new C++ Page object and store it in any newly created
+// Ruby page instances.
 VALUE page_allocate(VALUE rb_PDFShaver_Page) {
   Page* page = new Page();
   return Data_Wrap_Struct(rb_PDFShaver_Page, NULL, destroy_page, page);
+}
+// And delete the C++ page when we're done with the Ruby page.
+static void destroy_page(Page* page) { delete page; }
+
+// This function does the actual initialization of the C++ page's internals
+// defining which page of the document will be opened when `load_data` is called.
+VALUE initialize_page_internals(int arg_count, VALUE* args, VALUE self) {
+  // use Ruby's argument scanner to pull out a required
+  VALUE rb_document, page_index, options;
+  int number_of_args = rb_scan_args(arg_count, args, "21", &rb_document, &page_index, &options);
+  
+  // fetch the C++ document from the Ruby document the page has been initialized with
+  Document* document;
+  Data_Get_Struct(rb_document, Document, document);
+  // And fetch the C++ page
+  Page* page;
+  Data_Get_Struct(self, Page, page);
+  // and associate them by initializing the C++ page.
+  page->initialize(document, FIX2INT(page_index));
+  return self;
 }
 
 VALUE page_load_data(VALUE self) {
@@ -198,20 +232,3 @@ VALUE page_render(int arg_count, VALUE* args, VALUE self) {
   page_unload_data(self);
   return output;
 }
-
-VALUE initialize_page_internals(int arg_count, VALUE* args, VALUE self) {
-  // use Ruby's argument scanner to pull out a required
-  VALUE rb_document, page_index, options;
-  int number_of_args = rb_scan_args(arg_count, args, "21", &rb_document, &page_index, &options);
-  
-  Document* document;
-  Data_Get_Struct(rb_document, Document, document);
-  
-  Page* page;
-  Data_Get_Struct(self, Page, page);
-  
-  page->initialize(document, FIX2INT(page_index));
-  return self;
-}
-
-static void destroy_page(Page* page) { delete page; }
